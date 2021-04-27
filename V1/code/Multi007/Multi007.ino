bece0cb8fad73652d463ed8ddf75d4f12d328e23
Daniel Treffenstädt <daniel@treffenstaedt.de>
2021-04-27 12:26:03 +0200

    Simplified encoder direction checking.
    This makes it less error-prone and leads to less code-duplication.

--- V1/code/Multi007/Multi007.ino
+++ V1/code/Multi007/Multi007.ino
@@ -227,54 +227,18 @@ void loop() {
 
 }
 
-
-void doEncoderA(){  
-
-  // look for a low-to-high on channel A
-  if (digitalRead(encoder0PinA) == HIGH) { 
-    // check channel B to see which way encoder is turning
-    if (digitalRead(encoder0PinB) == LOW) {  
-      encoder0Pos = encoder0Pos + 1;         // CW
-    } 
-    else {
-      encoder0Pos = encoder0Pos - 1;         // CCW
+inline int encoder_parity()
+{
+    if (digitalRead(encoder0PinA) == digitalRead(encoder0PinB)) {
+        return 1;
     }
-  }
-  else   // must be a high-to-low edge on channel A                                       
-  { 
-    // check channel B to see which way encoder is turning  
-    if (digitalRead(encoder0PinB) == HIGH) {   
-      encoder0Pos = encoder0Pos + 1;          // CW
-    } 
-    else {
-      encoder0Pos = encoder0Pos - 1;          // CCW
-    }
-  }
- 
+    return -1;
 }
 
-void doEncoderB(){  
+void doEncoderA() {
+    encoder0Pos += - encoder_parity();
+}
 
-  // look for a low-to-high on channel B
-  if (digitalRead(encoder0PinB) == HIGH) {   
-   // check channel A to see which way encoder is turning
-    if (digitalRead(encoder0PinA) == HIGH) {  
-      encoder0Pos = encoder0Pos + 1;         // CW
-    } 
-    else {
-      encoder0Pos = encoder0Pos - 1;         // CCW
-    }
-  }
-  // Look for a high-to-low on channel B
-  else { 
-    // check channel B to see which way encoder is turning  
-    if (digitalRead(encoder0PinA) == LOW) {   
-      encoder0Pos = encoder0Pos + 1;          // CW
-    } 
-    else {
-      encoder0Pos = encoder0Pos - 1;          // CCW
-    }
-  }
-  
-
+void doEncoderB() {
+    encoder0Pos += encoder_parity();
 }
